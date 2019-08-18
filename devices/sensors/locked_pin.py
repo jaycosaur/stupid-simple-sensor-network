f290c2908165b89a89f90235fbdce9b2ffa738d4
jaycosaur <jaycorichter@gmail.com>
2019-08-18 23:00:33 +1000

    feat: add locked pin class

---
+++ devices/sensors/locked_pin.py
@@ -0,0 +1,34 @@
+from _thread import allocate_lock
+from machine import Pin
+
+
+class LockedPin(Pin):
+    '''
+    Thread safe pin for control across multiple threads.
+    Use with:
+    new_pin = ControlledPin(0)
+    with new_pin as pin:
+        pin.on()
+        time.sleep(5)
+        pin.off()
+    '''
+
+    def __init__(self, id, mode=-1, pull=-1, value=0):
+        _pin = Pin(id, mode, pull, value=value)
+        _lock = allocate_lock()
+
+    def __enter__(self):
+        self._lock.acquire()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._lock.release()
+
+    def value(self, state_value):
+        self._pin.value(state_value)
+
+    def on(self):
+        self._pin.on()
+
+    def off(self):
+        self._pin.off()
